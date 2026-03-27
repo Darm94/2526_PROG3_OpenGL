4b998fc8eb1030f2c8a9e9af5290870243197b36
Federico De Felici <fdf8210@gmail.com>
2026-03-27 15:00:14 +0100

    introduce Ex05

---
+++ include/Ex05QuadTextureDraw.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <glad/gl.h>
+
+class Ex05QuadTextureDraw 
+{
+public:
+    Ex05QuadTextureDraw();
+    ~Ex05QuadTextureDraw();
+    void Update(float InDeltaTime);
+private:
+    class OGLProgram* Program;
+    GLuint Vao;
+    GLuint Vbo;
+    GLuint Ebo;
+};
--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,7 @@
 #include "Ex02QuadDraw.h"
 #include "Ex03QuadIndexDraw.h"
 #include "Ex04QuadColorDraw.h"
+#include "Ex05QuadTextureDraw.h"
 
 int main() 
 {
@@ -13,7 +14,8 @@ int main()
 	//Ex01TriangleDraw Scene;
 	//Ex02QuadDraw Scene;
 	//Ex03QuadIndexDraw Scene;
-	Ex04QuadColorDraw Scene;
+	//Ex04QuadColorDraw Scene;
+	Ex05QuadTextureDraw Scene;
 
 	while(Window.IsOpened()) 
 	{
